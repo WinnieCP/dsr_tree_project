e859a4b83ba237cc11b8ac624f6ec64ca942b565
WinnieCP <winnie.poel@gmail.com>
2023-03-29 10:41:33 +0200

    prototype of streamlit app

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,11 +3,22 @@ import streamlit as st
 from streamlit_folium import folium_static
 import geemap.eefolium as geemap
 import ee
+from google.oauth2 import service_account
+from ee import oauth
 
+
+def get_auth():
+    service_account_keys = st.secrets['ee_keys']
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_keys, scopes=oauth.SCOPES)
+    ee.Initialize(credentials)
+    return 'successfully sync to GEE'
+    )
 # os.environ["EARTHENGINE_TOKEN"] == st.secrets["EARTHENGINE_TOKEN"]
 
 "# streamlit geemap demo"
-st.markdown('Source code: <https://github.com/giswqs/geemap-streamlit/blob/main/geemap_app.py>')
+#st.markdown('Source code: <https://github.com/giswqs/geemap-streamlit/blob/main/geemap_app.py>')
+st.title('Trees of Berlin')
 
 with st.echo():
     import streamlit as st
@@ -15,16 +26,14 @@ with st.echo():
     import geemap.eefolium as geemap
     import ee
 
-    m = geemap.Map()
-    dem = ee.Image('USGS/SRTMGL1_003')
+    map = geemap.Map()
+    image = ee.Image("Germany/Brandenburg/orthos/20cm")
+        .select(['R','G','B'])
+    map.addLayer(image, {}, "Earth Engine")
 
-    vis_params = {
-    'min': 0,
-    'max': 4000,
-    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
 
-    m.addLayer(dem, vis_params, 'SRTM DEM', True, 1)
-    m.addLayerControl()
+
+    map.addLayerControl()
 
     # call to render Folium map in Streamlit
-    folium_static(m)
+    folium_static(map)
