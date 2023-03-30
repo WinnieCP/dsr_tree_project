068e4ba09b8e1e8491dba58828b3ddb0f67db169
WinnieCP <winnie.poel@gmail.com>
2023-03-30 09:33:52 +0200

    bug fix

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -10,10 +10,9 @@ import json
 #json_data = st.secrets["json_data"]
 #service_account = st.secrets["service_account"]
 # Preparing values
-json_object = json.loads(json_data, strict=False)
-service_account = json_object['client_email']
-
-json_object = json.dumps(json_object)
+#json_object = json.loads(json_data, strict=False)
+#service_account = json_object['client_email']
+#json_object = json.dumps(json_object)
 
 # Authorising the app
 # credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
