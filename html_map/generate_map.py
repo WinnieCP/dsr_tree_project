3a420f0c6202a001f52884ad55f822a4fc0d4442
WinnieCP <winnie.poel@gmail.com>
2023-04-03 15:57:37 +0200

    all label boxes to width4

--- html_map/generate_map.py
+++ html_map/generate_map.py
@@ -24,7 +24,7 @@ ee.Initialize(credentials)
 
 center_pt = [13.420409,52.534824]#[13.42378, 52.535262]
 map = geemap.Map()
-map.setCenter(center_pt[0],center_pt[1],19)
+map.setCenter(center_pt[0],center_pt[1],18)
 
 # option to display also nrg image as layer:
 image_nrg = add_eelayer(map, channels=['N','R','G'])
@@ -42,7 +42,7 @@ projection = image_rgb.projection()
 
 # add validation labels
 df_reg = pd.read_csv('../data/Baumkataster_Prenzlauer_Berg.csv')
-registry_label_style = {'color': 'ffffffff ', 'width': 5, 'lineType': 'solid', 'fillColor': 'ffffff44'}
+registry_label_style = {'color': 'ffffffff ', 'width': 4, 'lineType': 'solid', 'fillColor': 'ffffff00'}
 registry_labels = ee.FeatureCollection([ee.Geometry.Point((x,y)).buffer(d/2).bounds() 
                               for x, y, d in zip(df_reg.X, 
                                                df_reg.Y,
@@ -58,6 +58,13 @@ d2_labels = ee.FeatureCollection([ee.Geometry.Point((x,y)).buffer(d/2).bounds()
                                                 df_d2.Y,
                                                df_d2.kronedurch)])
 map.addLayer(d2_labels.style(**d2_label_style),{}, 'Detectron2', shown=False)
+
+#d2_label_style = {'color': 'ff00a9ff', 'width': 3, 'lineType': 'solid', 'fillColor': '00000000'}
+#map.addLayer(d2_labels.style(**d2_label_style),{}, 'Detectron2 width3', shown=False)
+
+#d2_label_style = {'color': 'ff00a9ff', 'width': 2, 'lineType': 'solid', 'fillColor': '00000000'}
+#map.addLayer(d2_labels.style(**d2_label_style),{}, 'Detectron2 width2', shown=False)
+
 
 # add YOLO labels
 df_yolo = pd.read_csv('../data/Yolo_pred_Prenzlauer_Berg.csv')
